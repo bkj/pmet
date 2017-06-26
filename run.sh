183e1387e9048aa9e4b003a989d6a430b38b2171
bkj <ben@gophronesis.com>
2017-06-25 21:41:56 -0400

    ...

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 mkdir -p ./data
-python ./utils/fake-data.py --n 1000 > ./datatrain.jl
+python ./utils/fake-data.py --n 1000 > ./data/train.jl
 python ./utils/fake-data.py --n 1000 > ./data/test.jl
 
 python models/selfatt-model.py
